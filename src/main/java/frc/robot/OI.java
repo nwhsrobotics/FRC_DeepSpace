e666c3ce30d2b0db725eb95349587b2a15087aec
Ethan Martinez <eman2201@gmail.com>
2019-02-01 18:57:02 -0500

    got rid of unnecesasry imports

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -2,18 +2,9 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.SendableBase;
-import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
-import edu.wpi.first.wpilibj.command.Command;
 
 import frc.robot.commands.*;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 
 
@@ -45,16 +36,13 @@ public class OI{
   private final int LOWHATCH_BUTTON = 3;
   private final int CLAMP_BUTTON = 4;
   private final int ARM_BUTTON = 8;
- // private final int SLIDE_LEFT = lefttrigger;
- // private final int SLIDE_RIGHT = righttrigger;
+;
 
   public JoystickButton yButton2 = new JoystickButton(joy2, HIGHHATCH_BUTTON);
   public JoystickButton xButton2 = new JoystickButton(joy2, MIDHATCH_BUTTON);
   public JoystickButton aButton2 = new JoystickButton(joy2, LOWHATCH_BUTTON);
   public JoystickButton bButton2 = new JoystickButton(joy2, CLAMP_BUTTON);
   public JoystickButton startButton2 = new JoystickButton(joy2, ARM_BUTTON);
-//public JoystickButton triggerleft2 = new JoystickButton(joy2, SLIDE_LEFT);
-//public JoystickButton triggerright2 = new JoystickButton(joy2, SLIDE_RIGHT);
  /* Button b1 = new JoystickButton(joy2, 1);
   Button b4 = new JoystickButton(joy2, 4);
   */
