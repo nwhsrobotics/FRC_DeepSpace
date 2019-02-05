4dbadd1c1c8e0d7d65f29fadc7644f39184912fe
Ethan Martinez <eman2201@gmail.com>
2019-02-05 18:18:25 -0500

    began velocity control development

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -8,27 +8,47 @@
 package frc.robot.subsystems;
 
 import frc.robot.Robot;
-import frc.robot.RobotMap;
 import frc.robot.RobotMap.MapKeys;
+import frc.robot.commands.DriveCommand;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PIDSourceType;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import frc.robot.commands.*;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+import edu.wpi.first.wpilibj.Joystick.AxisType;
+import java.math.*;
+
 
 /**
  * Add your docs here.
  */
 public class DriveTrain extends Subsystem {
-  //create CAN Talon SRX objects
   private WPI_TalonSRX m_frontleft;
   private WPI_TalonSRX m_backleft;
   private WPI_TalonSRX m_frontright;
   private WPI_TalonSRX m_backright;
-  //create drive train and drive train sides objects
-  private SpeedControllerGroup m_left;
-  private SpeedControllerGroup m_right;
-  private DifferentialDrive m_drive;
+  private static final int TIMEOUT_MS = 1000;
+  //feet per second velocity
+  private static double m_velocity_fps;
+  private static double m_velocity_turn_rps;//rad per second
+  private static double m_left;
+  private static double m_right;
+  private static final double WHEELBASE = 15;
+  private static double m_radius = WHEELBASE / 2;
+
+
+  private static int m_maxAmps = 0;
+  private static double m_maxIntegral = 0;
+  private static final double COUNTSPERFOOT = 1800;
+  private static final double MAXVELOCITY = 5;
+  private static final double MAX_TURN_RATE_DEG_PER_SEC = 15;
+  private static final double RAD_PER_DEG = Math.PI/180;
 
 
 
@@ -36,30 +56,61 @@ public class DriveTrain extends Subsystem {
     //initialize + set objects created above
     m_frontleft = new WPI_TalonSRX(Robot.m_map.getId(MapKeys.DRIVE_FRONTLEFT));
     m_backleft = new WPI_TalonSRX(Robot.m_map.getId(MapKeys.DRIVE_BACKLEFT));
-    if ((m_frontleft != null) && (m_backleft != null)) {
-      m_left = new SpeedControllerGroup(m_frontleft, m_backleft);
-    } 
-
     m_frontright = new WPI_TalonSRX(Robot.m_map.getId(MapKeys.DRIVE_FRONTRIGHT));
     m_backright = new WPI_TalonSRX(Robot.m_map.getId(MapKeys.DRIVE_BACKRIGHT));
-    if ((m_frontright != null) && (m_backright != null)) {
-      m_right = new SpeedControllerGroup(m_frontright, m_backright);
-    }
-    //m_left.setInverted(true); invert left side
 
-    if ((m_left != null) && (m_right != null)) {
-      m_drive = new DifferentialDrive(m_left, m_right);
-    }
+    configTalons();
+    m_velocity_fps = 0;
+    m_velocity_turn_rps = 0;
+
   }
 
   public void configTalons() {
-    
+    m_frontright.selectProfileSlot(0, 0);
+    m_frontright.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+    m_frontright.setSensorPhase(false);
+
+    m_frontleft.selectProfileSlot(0, 0);
+    m_frontleft.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+    m_frontleft.setSensorPhase(false);
+    //_talon.configEncoderCodesPerRev(XXX), // if using FeedbackDevice.QuadEncoder
+    //_talon.configPotentiometerTurns(XXX), // if using FeedbackDevice.AnalogEncoder or AnalogPot
+
+	/* set closed loop gains in slot0 */
+    m_frontright.config_kP(0, 0, TIMEOUT_MS);
+    m_frontright.config_kI(0, 0, TIMEOUT_MS);
+    m_frontright.config_kD(0, 0, TIMEOUT_MS);
+    m_frontright.config_kF(0, 0, TIMEOUT_MS);
+
+    m_frontleft.config_kP(0, 0, TIMEOUT_MS);
+    m_frontleft.config_kI(0, 0, TIMEOUT_MS);
+    m_frontleft.config_kD(0, 0, TIMEOUT_MS);
+    m_frontleft.config_kF(0, 0, TIMEOUT_MS);
+
+    m_frontright.configMaxIntegralAccumulator(0, m_maxIntegral, TIMEOUT_MS);
+    m_frontright.configContinuousCurrentLimit(m_maxAmps, TIMEOUT_MS);
+    m_frontright.configPeakCurrentLimit(m_maxAmps, TIMEOUT_MS);
+
+    m_frontleft.configMaxIntegralAccumulator(0, m_maxIntegral, TIMEOUT_MS);
+    m_frontleft.configContinuousCurrentLimit(m_maxAmps, TIMEOUT_MS);
+    m_frontleft.configPeakCurrentLimit(m_maxAmps, TIMEOUT_MS);
+
+    m_backleft.set(ControlMode.Follower, Robot.m_map.getId(MapKeys.DRIVE_BACKLEFT));
+    m_backright.set(ControlMode.Follower, Robot.m_map.getId(MapKeys.DRIVE_BACKRIGHT));
   }
 
   public void update(double y, double z){
-    if (m_drive != null) {
-      m_drive.arcadeDrive(y, z);
-    }
+    m_velocity_fps = y * MAXVELOCITY;
+    m_velocity_turn_rps = -z * (MAX_TURN_RATE_DEG_PER_SEC/RAD_PER_DEG);
+
+    m_right = m_velocity_fps + m_velocity_turn_rps;
+    m_left = m_velocity_fps - m_velocity_turn_rps;
+  }
+
+  @Override
+  public void periodic() {
+    m_frontleft.set(ControlMode.Velocity, m_left);
+    m_frontleft.set(ControlMode.Velocity, m_right);
   }
 
 
